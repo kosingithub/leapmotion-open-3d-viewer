cbc311fd8d2d8ccf01c09a3b0e246cd0665c19e2
ilopez <ilopez@switchfly.com>
2014-04-20 22:57:46 -0700

    adds TODOs

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -40,7 +40,19 @@
             this.init(options);
             this._viewer = new exports.o3v.Viewer();
             /*TODO create fields objects that encapsulates several viewer objects and uses options
+            this.layersManager = new LayersManager(this._viewer,options);
+             the layersManager should have at least the following methods
+                  setLayerUiVisibility(:boolean)
+                  showLayers(layers:array)
+                  hideLayers(layers:array)
              this.navigator = new Navigator(this._viewer,options)
+                 the navigator should have at least the following methods
+                      moveModel[Left|Right|Down|Top]();
+                      moveCamera[Left|Right|Down|Top]();
+                      continuouslyMoveModel[Left|Right|Down|Top](velocity:float);
+                      stopModelMovement();
+                      enableSupermanMode();
+                      disableSupermanMode();
              */
         };
     };
