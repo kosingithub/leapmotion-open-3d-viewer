18987f3372de90db2935faf3f49ce1b489fdea78
rilopez <ilopez@switchfly.com>
2014-04-22 09:35:00 -0700

    implemented layers

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -42,6 +42,7 @@
             this.init(options);
             self._viewer = new exports.o3v.Viewer();
             self.navigator = new exports.Navigator(self._viewer,options);
+            self.layersManager = new exports.LayersManager(this._viewer,options);
 
             /*TODO create fields objects that encapsulates several viewer objects and uses options
               this.layersManager = new LayersManager(this._viewer,options);
