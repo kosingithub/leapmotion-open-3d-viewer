5cf58b8b592a947ebe49b640808abf8ad345ffc9
ariel <ariel_556@hotmail.com>
2014-04-22 22:44:31 -0700

    I've added all the actions of nav_ui file.

--- app/scripts/navigator.js
+++ app/scripts/navigator.js
@@ -3,31 +3,85 @@
 // this flag and the unused var should be removed!!
 (function(exports){
 	//'use strict';
-	var myVar=null;
+  var myVar=null;
 	exports.Navigator = function(viewer, options){
-       var viewerNavigator = viewer.navigator_, //
+           var viewerNavigator = viewer.navigator_, //
        move = viewerNavigator.drag.bind(viewerNavigator);
+       zoom = viewerNavigator.scroll.bind(viewerNavigator);
+       //TODO move the MOVE_FACTOR to the app configs
+                               //////////////
+                               //MoveModels//
+                               //////////////
        this.moveLeft = function (){
-           //TODO move the MOVE_FACTOR to the app configs
            move(-exports.o3v.navUI.MOVE_FACTOR, 0);
        }
        this.moveRight = function (){
-           //TODO move the MOVE_FACTOR to the app configs
            move(exports.o3v.navUI.MOVE_FACTOR, 0);
        }
+                               /////////////////////////
+                               //ContinuouslyMoveModel//
+                               /////////////////////////
        this.continuouslyMoveModelLeft = function (){
-       	continuouslyMoveModel(500,0,"-");
+        this.stopModelMovement();
+       	continuouslyMoveModel(move,-exports.o3v.navUI.MOVE_FACTOR,0);
        }
        this.continuouslyMoveModelRight = function (){
-       	continuouslyMoveModel(500,0,"");
+        this.stopModelMovement();
+       	continuouslyMoveModel(move,exports.o3v.navUI.MOVE_FACTOR,0);
        }
-       this.stopModelMovement =  function (){
-       	clearTimeout(myVar);
+                               //////////////
+                               //MoveCamera//
+                               //////////////
+        this.moveCameraTop = function (){
+           move(0, -o3v.navUI.MOVE_FACTOR);
        }
-       function continuouslyMoveModel(a,b,kind){
-       	move(kind+exports.o3v.navUI.MOVE_FACTOR, 0);
-       	b++;
-       	if(b<=a) myVar = setTimeout(function(){ continuouslyMoveModel(a,b,kind); },80);
+        this.moveCameraDown = function (){
+           move(0, o3v.navUI.MOVE_FACTOR);
+       }
+                               //////////////////////////
+                               //ContinuouslyMoveCamera//
+                               //////////////////////////
+        this.continuouslymoveCameraTop = function (){
+        this.stopModelMovement();
+        continuouslyMoveModel(move,0, -exports.o3v.navUI.MOVE_FACTOR);
+       }
+       this.continuouslymoveCameraDown = function (){
+        this.stopModelMovement();
+        continuouslyMoveModel(move,0, exports.o3v.navUI.MOVE_FACTOR);
+       }
+                               ////////////
+                               //MoveZoom//
+                               ////////////
+        //TODO move the ZOOM_FACTOR to the app configs
+        this.moveZoomIn = function (){
+          zoom(0, o3v.navUI.ZOOM_FACTOR);
+       }
+        this.moveZoomOut = function (){
+          zoom(0, -o3v.navUI.ZOOM_FACTOR);
+       }
+                               ////////////////////////
+                               //ContinuouslyMoveZoom//
+                               ////////////////////////
+        this.continuouslymoveZoomIn = function (){
+        this.stopModelMovement();
+        continuouslyMoveModel(zoom,0, exports.o3v.navUI.MOVE_FACTOR);
+       }
+       this.continuouslymoveZoomOut = function (){
+        this.stopModelMovement();
+        continuouslyMoveModel(zoom,0, -exports.o3v.navUI.MOVE_FACTOR);
+        
+       }
+                               ////////////////////
+                               //DynamicMoveModel//
+                               ////////////////////
+        this.stopModelMovement =  function (){
+        clearTimeout(myVar);
+       }
+        function continuouslyMoveModel(functions,kind1,kind2,starts){
+        if(starts===undefined) starts=0;
+        functions(kind1, kind2);
+        if(starts<=500) myVar = setTimeout(function(){ continuouslyMoveModel(functions,kind1,kind2,++starts); },80);
        }
    } 
 })(window);
+
