d9d44c06e5aa2798714921afcace566992cf9c56
Jesus Castro <jcastro@nearsoft.com>
2014-04-23 17:20:02 -0700

    added getLayer() and modified the layers access.

--- app/scripts/layers_manager.js
+++ app/scripts/layers_manager.js
@@ -3,43 +3,51 @@
 
     exports.LayersManager = function (viewer) {
         var _layerOpacityManager = viewer.layerOpacityManager_;
+        var _layers = {
+            SKIN:   6,
+            MUSCLE: 5,
+            BONES:  4,
+            ORGANS: 3,
+            CIRCULATORY_SYSTEM: 2,
+            LYMPHATIC_SYSTEM:   1,
+            NERVOUS_SYSTEM:     0
+        }
+        this.getLayer = function (layer){
+            return _layers[layer];
+        };
 
-        this.SKIN = 6;
-        this.MUSCLE = 5;
-        this.BONES = 4;
-        this.ORGANS = 3;
-        this.CIRCULATORY_SYSTEM = 2;
-        this.LYMPHATIC_SYSTEM = 1;
-        this.NERVOUS_SYSTEM = 0;
+        this.setLayerUiVisibility = function (isVisible)
+        {
+            if (isVisible) {
+                //set visibility to ALL
+            };
+        }
 
-        this.hideLayers = function (layers) {
+        this.hideLayers = function (layers)
+        {
             var layer,  //
-                updateCallback = function () {
+              updateCallback = function () {
                     console.log('hidden layer : ' + layer);
                 };
-            for (var i = 0; i < layers.length; i++) {
-                layer = layers[i];
-                _layerOpacityManager.setLayerOpacity(
-                    layer, 0 / 10000,updateCallback);
-
-            }
+              for (var i = 0; i < layers.length; i++) {
+                 layer = layers[i];
+                _layerOpacityManager.setLayerOpacity(layer, 0 / 10000, updateCallback);
+           }
         };
+
         this.showLayers = function (layers) {
             var layer,  //
-                updateCallback = function () {
+              updateCallback = function () {
                     console.log('hidden layer : ' + layer);
                 };
-            //TODO implemnt this loop only once, this code is similar to the hide layers
-            for (var i = 0; i < layers.length; i++) {
+            //TODO implement this loop only once, this code is similar to the hide layers
+              for (var i = 0; i < layers.length; i++) {
                 layer = layers[i];
-                _layerOpacityManager.setLayerOpacity(
-                    layer, 10000 / 10000, updateCallback);
-            }
+                _layerOpacityManager.setLayerOpacity(layer, 10000 / 10000, updateCallback);
+           }
+          };
         };
-    };
-})
-
-    (window);
+      })(window);
 
 //this.layersManager = new LayersManager(this._viewer,options);
 //the layersManager should have at least the following methods
